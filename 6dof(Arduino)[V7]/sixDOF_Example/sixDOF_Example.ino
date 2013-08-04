c6639d14e10cbd08d944896c43e4fd66dd29b49a
Oliver <oliver@azul.me>
2013-08-03 20:33:06 -0500

    First off, wow, I didn't add this? Second off, I changed the code to convert the values from the ardiuno to the intended g values (values in terms of the force of gravity). Now we should be able to ros bag this and get data that makes sense. Yay.

---
+++ 6dof(Arduino)[V7]/sixDOF_Example/sixDOF_Example.ino
@@ -0,0 +1,34 @@
+#include <FreeSixIMU.h>
+#include <FIMU_ADXL345.h>
+#include <FIMU_ITG3200.h>
+
+#include <Wire.h>
+float conv = 0.00390625;
+float angles[3]; // yaw pitch roll
+float allValues[6];
+// Set the FreeSixIMU object
+FreeSixIMU sixDOF = FreeSixIMU();
+
+void setup() { 
+  Serial.begin(115200);
+  Wire.begin();
+  
+  delay(5);
+  sixDOF.init(); //begin the IMU
+  delay(5);
+}
+
+void loop() { 
+  
+  sixDOF.getValues(allValues);
+  Serial.print("Velocities ");
+  Serial.print(allValues[0] * conv);
+  Serial.print(" | ");  
+  Serial.print(allValues[1] * conv);
+  Serial.print(" | ");
+  Serial.println(allValues[2] * conv);
+  
+  
+  delay(100); 
+}
+
